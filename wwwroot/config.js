d47ec84be094b37e6a90ae3c0af2eaf2f2ce082f
Douglas Ludlow <doug.ludlow@royaljay.net>
2016-01-25 15:43:05 -0700

    Add ts plugin and config.

--- wwwroot/config.js
+++ wwwroot/config.js
@@ -2,11 +2,31 @@ System.config({
   baseURL: "/",
   defaultJSExtensions: true,
   transpiler: "typescript",
+  typescriptOptions: {
+    "tsconfig": true,
+    "module": "system"
+  },
   paths: {
-    "npm:*": "jspm_packages/npm/*"
+    "npm:*": "jspm_packages/npm/*",
+    "github:*": "jspm_packages/github/*"
+  },
+
+  packages: {
+    "app": {
+      "defaultExtension": "ts",
+      "meta": {
+        "*.ts": {
+          "loader": "ts"
+        }
+      }
+    }
   },
 
   map: {
-    "typescript": "npm:typescript@1.7.5"
+    "ts": "github:frankwallis/plugin-typescript@2.5.2",
+    "typescript": "npm:typescript@1.7.5",
+    "github:frankwallis/plugin-typescript@2.5.2": {
+      "typescript": "npm:typescript@1.7.5"
+    }
   }
 });
